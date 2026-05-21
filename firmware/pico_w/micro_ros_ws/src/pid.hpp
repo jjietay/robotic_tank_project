8aaf0cd370bb824ddc34eeb70241677108692468
jjietay <tayjuenjie.tjj@gmail.com>
2026-05-21 23:38:14 +0800

    changed variable name in pid (no change to logic)
    updated correct naming for package

--- firmware/pico_w/micro_ros_ws/src/pid.hpp
+++ firmware/pico_w/micro_ros_ws/src/pid.hpp
@@ -25,8 +25,8 @@ private:
     float kp, ki, kd;
     float integral = 0.0f;
     float integral_max; // for clamping
-    float output_min;
-    float output_max;
+    float pwm_out_min;
+    float pwm_out_max;
     float previous_error = 0.0f;
     uint64_t last_time;
     bool first_run = true;
