28be2f8e2dd1ef05795c73b78de7d23ed33edf38
jjietay <tayjuenjie.tjj@gmail.com>
2026-06-12 16:47:20 +0800

    Change YDLIDAR output from screen to log to suppress warnings in terminal

--- software/ros2_ws/src/tank_bringup/launch/nav2.launch.py
+++ software/ros2_ws/src/tank_bringup/launch/nav2.launch.py
@@ -46,7 +46,7 @@ def generate_launch_description():
             package='ydlidar_ros2_driver',
             executable='ydlidar_ros2_driver_node',
             name='ydlidar_ros2_driver_node',
-            output='screen',
+            output='log',              # suppress "Real points > fixed points" warnings from terminal
             parameters=[ydlidar_params],
         ),
 
